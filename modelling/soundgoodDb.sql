5bc923f9c7fc0720b4bf527aea3175c45f27a5cd
neonguyen-dev <neo20030125@gmail.com>
2023-11-23 11:36:14 +0100

    soundgood script and model is updated to fit creating the database

--- modelling/soundgoodDb.sql
+++ modelling/soundgoodDb.sql
@@ -16,7 +16,7 @@ ALTER TABLE instrument_type ADD CONSTRAINT PK_instrument_type PRIMARY KEY (id);
 
 CREATE TABLE person (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- person_number VARCHAR(12) NOT NULL,
+ person_number VARCHAR(13) NOT NULL,
  first_name VARCHAR(500) NOT NULL,
  last_name VARCHAR(500) NOT NULL,
  street VARCHAR(500) NOT NULL,
@@ -43,7 +43,7 @@ ALTER TABLE pricing_scheme ADD CONSTRAINT PK_pricing_scheme PRIMARY KEY (id);
 
 CREATE TABLE contact_person (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ person_id INT NOT NULL
 );
 
 ALTER TABLE contact_person ADD CONSTRAINT PK_contact_person PRIMARY KEY (id);
@@ -52,7 +52,7 @@ ALTER TABLE contact_person ADD CONSTRAINT PK_contact_person PRIMARY KEY (id);
 CREATE TABLE instructor (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  employment_id VARCHAR(500) NOT NULL,
- person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ person_id INT NOT NULL
 );
 
 ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
@@ -62,7 +62,7 @@ CREATE TABLE instrument (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  brand VARCHAR(500),
  model VARCHAR(500) NOT NULL,
- instrument_type_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ instrument_type_id INT NOT NULL,
  stored_quantity VARCHAR(10) NOT NULL
 );
 
@@ -72,15 +72,15 @@ ALTER TABLE instrument ADD CONSTRAINT PK_instrument PRIMARY KEY (id);
 CREATE TABLE lesson (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  lesson_id VARCHAR(500),
- instrument_type_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ instrument_type_id INT NOT NULL,
  skill_level INT NOT NULL,
  type_of_lesson INT NOT NULL,
  minimum VARCHAR(500),
  maximum VARCHAR(500),
  time_slot TIMESTAMP(10) NOT NULL,
- genre_id INT GENERATED ALWAYS AS IDENTITY,
- instructor_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- pricing_scheme_id INT GENERATED ALWAYS AS IDENTITY
+ genre_id INT,
+ instructor_id INT NOT NULL,
+ pricing_scheme_id INT
 );
 
 ALTER TABLE lesson ADD CONSTRAINT PK_lesson PRIMARY KEY (id);
@@ -88,7 +88,7 @@ ALTER TABLE lesson ADD CONSTRAINT PK_lesson PRIMARY KEY (id);
 
 CREATE TABLE schedule (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- instructor_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ instructor_id INT NOT NULL,
  available_from TIMESTAMP(10),
  available_to TIMESTAMP(10)
 );
@@ -98,25 +98,25 @@ ALTER TABLE schedule ADD CONSTRAINT PK_schedule PRIMARY KEY (id,instructor_id);
 
 CREATE TABLE student (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ person_id INT NOT NULL,
  student_id VARCHAR(500) NOT NULL,
- contact_person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ contact_person_id INT NOT NULL
 );
 
 ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
 
 
 CREATE TABLE student_lesson (
- student_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- lesson_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ student_id INT NOT NULL,
+ lesson_id INT NOT NULL
 );
 
 ALTER TABLE student_lesson ADD CONSTRAINT PK_student_lesson PRIMARY KEY (student_id,lesson_id);
 
 
 CREATE TABLE teaching_instruments (
- instructor_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- instrument_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ instructor_id INT NOT NULL,
+ instrument_id INT NOT NULL
 );
 
 ALTER TABLE teaching_instruments ADD CONSTRAINT PK_teaching_instruments PRIMARY KEY (instructor_id,instrument_id);
@@ -124,11 +124,11 @@ ALTER TABLE teaching_instruments ADD CONSTRAINT PK_teaching_instruments PRIMARY 
 
 CREATE TABLE rental (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- instrument_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ instrument_id INT NOT NULL,
  start_date DATE NOT NULL,
  end_date DATE NOT NULL,
  monthly_fee DECIMAL(10,2) NOT NULL,
- student_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ student_id INT NOT NULL,
  terminated BIT(1) NOT NULL
 );
 
@@ -136,8 +136,8 @@ ALTER TABLE rental ADD CONSTRAINT PK_rental PRIMARY KEY (id);
 
 
 CREATE TABLE sibling (
- student_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- sibling_id INT GENERATED ALWAYS AS IDENTITY NOT NULL
+ student_id INT NOT NULL,
+ sibling_id INT NOT NULL
 );
 
 ALTER TABLE sibling ADD CONSTRAINT PK_sibling PRIMARY KEY (student_id,sibling_id);
